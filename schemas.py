5c0bdc57cff2c848b241cee55cce4a6fb4a8070b
LLIaMMaH <abratunets@gmail.com>
2024-08-12 21:57:09 +0300

    fix: model config

--- schemas.py
+++ schemas.py
@@ -1,6 +1,6 @@
 from typing import Optional
 
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
 
 
 class SMemoAdd(BaseModel):
@@ -10,6 +10,8 @@ class SMemoAdd(BaseModel):
 
 class SMemo(SMemoAdd):
     id: int
+
+    model_config = ConfigDict(from_attributes=True)
 
 
 class SMemeId(BaseModel):
